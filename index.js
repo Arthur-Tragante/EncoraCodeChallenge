322969fcd7718153c6b11beb4f6899f63bb6e5b3
Arthur <arthur.tragante@gmail.com>
2022-12-01 16:18:28 -0300

    return words instead of numbers

--- index.js
+++ index.js
@@ -31,15 +31,35 @@ let ten = [
   "ninety",
 ];
 let scales = ["million", "thousand", "hundred"];
-
-amount = "5436843";
-
+let arrayOfWords = [];
+amount = "1111111";
+let dollarsArr = [];
 dollars = ("0000000" + amount)
   .slice(-7)
   .match(/^(\d{1})(\d{1})(\d{2})(\d{1})(\d{2})$/);
-
+// console.log(dollars);
 function write() {
-  console.log(dollars);
+  return dollars.map((value, index) => {
+    if (index === 1 && value != 0) {
+      return `${basics[value]}${scales[index - 1]} `;
+    } else if (index === 2 && value != 0) {
+      return `${basics[value]}${scales[index]}`;
+    } else if (index === 3 && value < 10 && value != 0) {
+      return `${basics[value[1]]}${scales[index - 2]}`;
+    } else if (index === 3 && value != 0 && value > 20) {
+      return `${ten[value[0] - 2]} ${basics[value[1]]}${scales[index - 2]} `;
+    } else if (index === 3 && value != 0 && value > 10 && value < 20) {
+      return `${basics[value]}${scales[index - 2]} `;
+    } else if (index === 4 && value != 0) {
+      return `${basics[value]}${scales[index - 2]} `;
+    } else if (index === 5 && value < 10 && value != 0) {
+      return `${basics[value[1]]}`;
+    } else if (index === 5 && value != 0 && value > 20) {
+      return `${ten[value[0] - 2]} ${basics[value[1]]} `;
+    } else if (index === 5 && value != 0 && value > 10 && value < 20) {
+      return `${basics[value]}`;
+    }
+  });
 }
 
-write();
+console.log(write());
